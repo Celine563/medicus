f164c6bac678699ebc941c5b3a24e1445bbc1d34
Celine <B00160080@mytudublin.ie>
2025-08-12 14:47:34 +0100

    v1

---
+++ V1/HomePage.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<link rel="stylesheet" type="text/css" href="CSS/HomeStyle.css">
+
+</head>
+
+<body>
+
+
+<div class="categories">
+    <h1><a href="HomePage.php">Medicus</a></h1>
+    
+    <div class="dropdown">
+    <h3>Appointments</h3>
+    <ul class="dropdown-menu">
+        <li><a href="#">X-ray</a></li>
+        <li><a href="#"></a></li>
+        <li><a href="#"></a></li>
+    </ul>
+</div>
+
+
+    <div class="dropdown">
+        <h3>Services</h3>
+        <ul class="dropdown-menu">
+            <li><a href="#"></a></li>
+            <li><a href="#"></a></li>
+            <li><a href="#"></a></li>
+        </ul>
+    </div>
+
+    <div class="dropdown">
+        <h3>About us</h3>
+        <ul class="dropdown-menu">
+            <li><a href="#"></a></li>
+            <li><a href="#"></a></li>
+        </ul>
+    </div>
+
+    <div class="dropdown">
+        <h3>Services</h3>
+        <ul class="dropdown-menu">
+            <li><a href="#"></a></li>
+            <li><a href="#">/a></li>
+        </ul>
+    </div>
+
+    <div class="dropdown">
+        <h3>Contact</h3>
+        <ul class="dropdown-menu">
+            <li><a href="#"></a></li>
+            <li><a href="#"></a></li>
+        </ul>
+    </div>
+
+
+    <img src="Images/Search.png" alt="search" class="search">
+    <img src="Images/Account.png" alt="account" class="account">
+</div>
+
+
+
+
+</body>
+</html>

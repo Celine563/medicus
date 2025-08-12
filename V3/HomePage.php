b80faad376a0a8d4a4e61e47f94368d20e2fc99f
Celine <B00160080@mytudublin.ie>
2025-08-12 18:47:09 +0100

    font

--- V3/HomePage.php
+++ V3/HomePage.php
@@ -6,15 +6,25 @@ require_once 'Util/connection.php';
 <!DOCTYPE html>
 <html>
 <head>
-  <link rel="stylesheet" href="CSS/HomeStyle.css">
+  <link rel="stylesheet" href="CSS/homePage.css">
 </head>
 <body>
 
 <?php require 'header.php'; ?>
 
 <br>
-<br>
 
+<div class="welcome">
+<h2>Welcome to Medicus</h2>
+<p>Medicus is here for you anytime with our 24/7 online call service, offering immediate support <br>
+ whenever you need it. For face-to-face care, our clinic is open Monday through Saturday from <br>
+ 9:00 AM to 10:00 PM. Whether it's day or night, your well-being is always our priority. </p>
+</div>
+
+<img src="Images/doctor.png" alt="doctor" class="doctor">
+
+<br>
+<br>
 <?php require 'footer.php'; ?>
 <?php require 'social.php'; ?>
 

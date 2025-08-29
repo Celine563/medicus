9e70a65d046594f78fa519d8b26b8594723c920f
Celine <B00160080@mytudublin.ie>
2025-08-30 00:46:47 +0100

    login

--- V3/account.php
+++ V3/account.php
@@ -12,12 +12,23 @@ require_once 'Util/connection.php';
 
 <?php require 'header.php'; ?>
 <br>
+<br>
 
   <div class="box">
-    login
+    <h1 class="title">LOGIN</h1>
+
+    <div class="login">
+      <form action="login.php" method="POST">
+              <input type="username" name="username" placeholder="Your email" required>
+              <input type="password" name="password" placeholder="Your password" required>
+              <button type="submit">Subscribe</button>
+          </form>
+          </div>
+
+
   </div>
 
-
+<br>
 <br>
 <?php require 'footer.php'; ?>
 <br>

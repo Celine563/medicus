e1ce5f59e07ab56152fd56a1c95ff621817b2e32
Celine <B00160080@mytudublin.ie>
2025-08-30 00:23:33 +0100

    + css

--- V3/account.php
+++ V3/account.php
@@ -6,14 +6,19 @@ require_once 'Util/connection.php';
 <!DOCTYPE html>
 <html>
 <head>
-  <link rel="stylesheet" type="text/css" href="CSS/homePage.css">
+  <link rel="stylesheet" type="text/css" href="CSS/account.css">
 </head>
 <body>
 
 <?php require 'header.php'; ?>
+<br>
+
+  <div class="box">
+    login
+  </div>
 
 
-
+<br>
 <?php require 'footer.php'; ?>
 <br>
 <br>
